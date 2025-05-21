497cbda2554fba95d3718324fc9b5ec972eb23f1
susnezhu <susan.klimo@gmail.com>
2025-05-21 11:34:23 +0300

    buyin & adding ingredients btn

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ if (canvas.getContext){
         ctx.closePath();
     }
 }
+
+//aineksen numero ja sen määrä
+const ingredients = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0,}
+
+//funktio saa aineksen arvon ja ostaa sen
+function buyIngredient(num) {
+    let quantity_text = document.getElementById("ingredient_quantity_" + num);
+    ingredients[num] += 1;
+    let quantity = ingredients[num];
+    quantity_text.textContent = quantity + " kpl";
+}
+
+//functio saa aineksen arvon ja lisää sen sekoitus alueelle
+function addIngredient(num) {
+    let quantity_text = document.getElementById("ingredient_quantity_" + num)
+    ingredients[num] -= 1;
+    let quantity = ingredients[num];
+    quantity_text.textContent = quantity + " kpl";
+}
